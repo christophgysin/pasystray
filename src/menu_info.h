02f70cf693463ee81d6a6a7a8cf59800be5fb032
Christoph Gysin <christoph.gysin@gmail.com>
2011-10-19 02:13:38 +0300

    add copyright and LGPL disclaimer to all files

--- src/menu_info.h
+++ src/menu_info.h
@@ -1,3 +1,24 @@
+/***
+  This file is part of PaSystray
+
+  Copyright 2011 Christoph Gysin
+
+  PaSystray is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) any later version.
+
+  PaSystray is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with PaSystray; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
+
 #ifndef PASYSTRAY_MENU_INFO_H
 #define PASYSTRAY_MENU_INFO_H
 
