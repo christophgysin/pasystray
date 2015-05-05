a464bad69e1d8eef277c957660023c2add2eb800
Christoph Gysin <christoph.gysin@gmail.com>
2015-05-05 21:47:09 +0300

    update copyright

--- src/avahi.h
+++ src/avahi.h
@@ -1,7 +1,7 @@
 /***
   This file is part of PaSystray
 
-  Copyright (C) 2011-2013  Christoph Gysin
+  Copyright (C) 2011-2015  Christoph Gysin
 
   PaSystray is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
