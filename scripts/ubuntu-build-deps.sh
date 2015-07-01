51f9267f65e431f8f8dee9ccc34cb2aac2340826
Christoph Gysin <christoph.gysin@gmail.com>
2015-07-01 23:19:59 +0300

    add script to install ubuntu build deps

---
+++ scripts/ubuntu-build-deps.sh
@@ -0,0 +1,4 @@
+apt-get install \
+    build-essential automake autoconf pkg-config \
+    libgtk-3-dev libpulse-dev libpulse-mainloop-glib0 \
+    libnotify-dev libavahi-glib-dev libappindicator3-dev
