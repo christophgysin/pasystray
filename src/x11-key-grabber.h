367b0b4d4902a9361c121b2216695e42adbbcb0f
Łukasz Stelmach <stlman@poczta.fm>
2021-05-13 11:00:27 +0200

    Import X11 volume control key grabber from pa-applet

---
+++ src/x11-key-grabber.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of pa-applet.
+ *
+ * © 2012 Fernando Tarlá Cardoso Lemos
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef KEY_GRABBER_H
+#define KEY_GRABBER_H
+
+typedef void (*key_grabber_cb)(void);
+
+void key_grabber_grab_keys(void);
+void key_grabber_ungrab_keys(void);
+void key_grabber_register_volume_raise_callback(key_grabber_cb cb);
+void key_grabber_register_volume_lower_callback(key_grabber_cb cb);
+void key_grabber_register_volume_mute_callback(key_grabber_cb cb);
+
+#endif
