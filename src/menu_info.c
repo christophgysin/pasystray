christophgysin/pasystray

commit acea1adb324103ff5f794e1ce084ff8fa8c487a0

listen to activate events of menu items
The new `activate` handlers substitute the current click handlers for default left click events, which do not get issued on Ubuntu using AppIndicators.

src/menu_info.c
@@ -369,6 +369,8 @@ void menu_info_item_add(menu_info_t* mi, uint32_t index, const char* name,
             break;
     }
 
+    g_signal_connect(item->widget, "activate",
+            G_CALLBACK(menu_info_item_activated), item);
     g_signal_connect(item->widget, "button-press-event",
             G_CALLBACK(menu_info_item_clicked), item);
     gtk_widget_add_events(item->widget, GDK_SCROLL_MASK);
@@ -448,6 +450,9 @@ void menu_info_subitem_add(menu_info_t* mi, uint32_t index, const char* name,
 
     g_signal_connect(subitem->widget, "button-press-event",
             G_CALLBACK(menu_info_subitem_clicked), subitem);
+
+    g_signal_connect(subitem->widget, "activate",
+            G_CALLBACK(menu_info_subitem_activated), subitem);
 }
 
 void menu_info_subitem_update(menu_info_t* mi, uint32_t index, const char* name,
@@ -498,6 +503,23 @@ gboolean desc_equal(gpointer key, gpointer value, gpointer user_data)
 menu_info_item_t* menu_info_item_get_by_desc(menu_info_t* mi, const char* desc)
 {
     return g_hash_table_find(mi->items, desc_equal, (gpointer)desc);
+}
+
+void menu_info_item_activated(GtkWidget* item, menu_info_item_t* mii)
+{
+    if (GTK_IS_CHECK_MENU_ITEM(item)
+            && !gtk_check_menu_item_get_active(GTK_MENU_ITEM(item)))
+    {
+        /* Ignore activation of deselected item  */
+        return;
+    }
+
+    g_debug("[menu_info] subitem activated: %s %s",
+            menu_info_type_name(mii->menu_info->type),
+            mii->name);
+
+    /* on left-click, set device as default */
+    pulseaudio_set_default(mii);
 }
 
 void menu_info_item_clicked(GtkWidget* item, GdkEventButton* event,
@@ -573,13 +595,18 @@ void menu_info_item_scrolled(GtkWidget* item, GdkEventScroll* event,
     }
 }
 
-void menu_info_subitem_clicked(GtkWidget* item, GdkEventButton* event,
-        menu_info_item_t* mii)
-{
-    g_debug("[menu_info] subitem clicked mod:%s%s button:%i",
-            (event->state & GDK_CONTROL_MASK) ? "[ctrl]" : "",
-            (event->state & GDK_MOD1_MASK) ? "[alt]" : "",
-            event->button);
+void menu_info_subitem_activated(GtkWidget* item, menu_info_item_t* mii) {
+
+    if (GTK_IS_CHECK_MENU_ITEM(item)
+            && !gtk_check_menu_item_get_active(GTK_MENU_ITEM(item)))
+    {
+        /* Ignore activation of deselected item  */
+        return;
+    }
+
+    g_debug("[menu_info] subitem activated: %s %s",
+            menu_info_type_name(mii->menu_info->type),
+            mii->name);
 
     switch(mii->menu_info->type)
     {
@@ -595,6 +622,15 @@ void menu_info_subitem_clicked(GtkWidget* item, GdkEventButton* event,
             pulseaudio_move_output_to_source(mii->menu_info->parent, mii);
             break;
     }
+}
+
+void menu_info_subitem_clicked(GtkWidget* item, GdkEventButton* event,
+        menu_info_item_t* mii)
+{
+    g_debug("[menu_info] subitem clicked mod:%s%s button:%i",
+            (event->state & GDK_CONTROL_MASK) ? "[ctrl]" : "",
+            (event->state & GDK_MOD1_MASK) ? "[alt]" : "",
+            event->button);
 }
 
 void menu_info_item_move_all_cb(GtkWidget* item, GdkEventButton* event, void* userdata)

src/menu_info.h
@@ -127,10 +127,12 @@ menu_info_item_t* menu_info_item_get(menu_info_t* mi, uint32_t index);
 menu_info_item_t* menu_info_item_get_by_name(menu_info_t* mi, const char* name);
 menu_info_item_t* menu_info_item_get_by_desc(menu_info_t* mi, const char* desc);
 
+void menu_info_item_activated(GtkWidget* item, menu_info_item_t* mii);
 void menu_info_item_clicked(GtkWidget* item, GdkEventButton* event,
         menu_info_item_t* mii);
 void menu_info_item_scrolled(GtkWidget* item, GdkEventScroll* event,
         menu_info_item_t* mii);
+void menu_info_subitem_activated(GtkWidget* item, menu_info_item_t* mii);
 void menu_info_subitem_clicked(GtkWidget* item, GdkEventButton* event,
         menu_info_item_t* mii);
 

commit f2628f184eccb5df9543f02df6981c34fcb441df

Fix spurious "[menu_info] item activated: input (null)"
These would happen when descending into playback streams submenus. The
fix is to ignore activation for anything that isn't a CHECK_MENU_ITEM.

(Also fixes related compile warning and debug logging.)

Fixes: acea1adb3241 ("listen to activate events of menu items")

src/menu_info.c
@@ -507,14 +507,14 @@ menu_info_item_t* menu_info_item_get_by_desc(menu_info_t* mi, const char* desc)
 
 void menu_info_item_activated(GtkWidget* item, menu_info_item_t* mii)
 {
-    if (GTK_IS_CHECK_MENU_ITEM(item)
-            && !gtk_check_menu_item_get_active(GTK_MENU_ITEM(item)))
+    if (!GTK_IS_CHECK_MENU_ITEM(item)
+            || !gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(item)))
     {
-        /* Ignore activation of deselected item  */
+        /* Ignore activation of submenus and deselected items */
         return;
     }
 
-    g_debug("[menu_info] subitem activated: %s %s",
+    g_debug("[menu_info] item activated: %s %s",
             menu_info_type_name(mii->menu_info->type),
             mii->name);
 
@@ -597,10 +597,10 @@ void menu_info_item_scrolled(GtkWidget* item, GdkEventScroll* event,
 
 void menu_info_subitem_activated(GtkWidget* item, menu_info_item_t* mii) {
 
-    if (GTK_IS_CHECK_MENU_ITEM(item)
-            && !gtk_check_menu_item_get_active(GTK_MENU_ITEM(item)))
+    if (!GTK_IS_CHECK_MENU_ITEM(item)
+            || !gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(item)))
     {
-        /* Ignore activation of deselected item  */
+        /* Ignore activation of submenus and deselected items */
         return;
     }
 